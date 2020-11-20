e3e901baf7da69b25c52d4463a78688400b66f8a
Elre <vze.ezv@gmail.com>
2020-11-20 16:30:23 +0000

    Removed a bunch of old adversarial code. Cleaned up some of the data preparation.

--- learners/cnaps/src/run_cnaps.py
+++ learners/cnaps/src/run_cnaps.py
@@ -47,7 +47,7 @@ import numpy as np
 import argparse
 import os
 from normalization_layers import TaskNormI
-from utils import print_and_log, write_to_log, get_log_files, ValidationAccuracies, loss, aggregate_accuracy, verify_checkpoint_dir
+from utils import print_and_log, write_to_log, get_log_files, ValidationAccuracies, loss, aggregate_accuracy, verify_checkpoint_dir, SavedDataset
 from model import Cnaps
 from meta_dataset_reader import MetaDatasetReader, SingleDatasetReader
 
@@ -112,21 +112,19 @@ class Learner:
         self.device = torch.device(gpu_device if torch.cuda.is_available() else 'cpu')
         self.model = self.init_model()
         self.train_set, self.validation_set, self.test_set = self.init_data()
-        # Must have at least one
-        assert self.args.target_set_size_multiplier >= 1
-        num_target_sets = self.args.target_set_size_multiplier
-        if self.args.indep_eval or self.args.swap_attack:
-            num_target_sets += NUM_INDEP_EVAL_TASKS
+
         if self.args.dataset == "meta-dataset":
             if self.args.query_test * self.args.target_set_size_multiplier > 50:
                 print_and_log(self.logfile, "WARNING: Very high number of query points requested. Query points = query_test * target_set_size_multiplier = {} * {} = {}".format(self.args.query_test, self.args.target_set_size_multiplier, self.args.query_test * self.args.target_set_size_multiplier))
 
             self.dataset = MetaDatasetReader(self.args.data_path, self.args.mode, self.train_set, self.validation_set,
                                              self.test_set, self.args.max_way_train, self.args.max_way_test,
-                                             self.args.max_support_train, self.args.max_support_test, self.args.query_test * num_target_sets)
+                                             self.args.max_support_train, self.args.max_support_test, self.args.query_test)
         elif self.args.dataset != "from_file":
             self.dataset = SingleDatasetReader(self.args.data_path, self.args.mode, self.args.dataset, self.args.way,
-                                               self.args.shot, self.args.query_train, self.args.query_test * num_target_sets)
+                                               self.args.shot, self.args.query_train, self.args.query_test)
+        else:
+            self.dataset = SavedDataset(self.args.data_path)
 
         self.loss = loss
         self.accuracy_fn = aggregate_accuracy
@@ -197,8 +195,6 @@ class Learner:
                             default="basic", help="Normalization layer to use.")
         parser.add_argument("--training_iterations", "-i", type=int, default=110000,
                             help="Number of meta-training iterations.")
-        parser.add_argument("--attack_tasks", "-a", type=int, default=10,
-                            help="Number of tasks when performing attack.")
         parser.add_argument("--val_freq", type=int, default=10000, help="Number of iterations between validations.")
         parser.add_argument("--max_way_train", type=int, default=40,
                             help="Maximum way of meta-dataset meta-train task.")
@@ -215,20 +211,8 @@ class Learner:
                             help="Shots per class for target  of single dataset task.")
         parser.add_argument("--query_test", type=int, default=10,
                             help="Shots per class for target  of single dataset task.")
-        parser.add_argument("--swap_attack", default=False,
-                            help="When attacking, should the attack be a swap attack or not.")
-        # Currently, target_set_size_multiplier only applies to swap attacks
-        parser.add_argument("--target_set_size_multiplier", type=int, default=1,
-                            help="For swap attacks, the relative size of the target set used when generating the adv context set (eg. x times larger). Currently only implemented for swap attacks")
-        # Currently only implemented for non-swap attacks
-        parser.add_argument("--save_attack", default=False,
+        parser.add_argument("--save_task", default=False,
                             help="Save all the tasks and adversarial images to a pickle file. Currently only applicable to non-swap attacks.")
-        parser.add_argument("--continue_from_task", type=int, default=0,
-                            help="When saving out large numbers of tasks one by one, this allows us to continue labelling new tasks from a certain point")
-        parser.add_argument("--save_samples", default=False,
-                            help="Output samples of the clean and adversarial images")
-        parser.add_argument("--indep_eval", default=False,
-                            help="Whether to use independent target sets for evaluation automagically")
         parser.add_argument("--do_not_freeze_feature_extractor", dest="do_not_freeze_feature_extractor", default=False,
                             action="store_true", help="If True, don't freeze the feature extractor.")
         args = parser.parse_args()
@@ -284,17 +268,6 @@ class Learner:
 
             if self.args.mode == 'test':
                 self.test_leave_one_out(self.args.test_model_path, session)
-
-            if self.args.mode == 'attack':
-                if not self.args.swap_attack:
-                    self.attack_homebrew(self.args.test_model_path, session)
-                elif self.args.dataset == "from_file":
-                    self.vary_swap_attack(self.args.test_model_path, session)
-                elif self.args.dataset == "meta-dataset":
-                    self.meta_dataset_attack_swap(self.args.test_model_path, session)
-                else:
-                    self.attack_swap(self.args.test_model_path, session)
-
 
             self.logfile.close()
 
@@ -352,9 +325,6 @@ class Learner:
             print_and_log(self.logfile, 'True accuracy: {0:3.5f}'.format(true_accuracy))
             print_and_log(self.logfile, 'Class Labels: {}'.format(context_labels))
 
-
-            #import pdb; pdb.set_trace()
-
             accuracy_loo = []
             for i in range(context_images.shape[0]):
                 if i == 0:
@@ -366,7 +336,7 @@ class Learner:
 
                 logits = self.model(context_images_loo, context_labels_loo, target_images)
                 accuracy = self.accuracy_fn(logits, target_labels).item()
-                #import pdb; pdb.set_trace()
+
                 print_and_log(self.logfile, 'Loo {0:} accuracy: {1:3.5f}'.format(i, true_accuracy - accuracy))
                 accuracy_loo.append(accuracy)
                 for c in range(0, self.args.way):
@@ -375,9 +345,8 @@ class Learner:
                     logits = self.model(context_images_loo, context_labels_loo, target_images_c)
                     accuracy = self.accuracy_fn(logits, target_labels_c).item()
                     print_and_log(self.logfile, '\tLoo {0:} class {1:}  accuracy: {2:3.5f}'.format(i, c, true_accuracy - accuracy))
-
-
                 del logits
+
 
 
     def test(self, path, session):
@@ -420,110 +389,9 @@ class Learner:
         save_image(adv_img.cpu().detach().numpy(),os.path.join(self.checkpoint_dir, 'adv_task_{}_index_{}.png'.format(task_no, index)))
         save_image(clean_img, os.path.join(self.checkpoint_dir, 'in_task_{}_index_{}.png'.format(task_no, index)))
 
-    def vary_swap_attack(self, path, session):
-        write_to_log(self.logfile, 'Attacking model {0:}: '.format(path))
-        self.model = self.init_model()
-        self.model.load_state_dict(torch.load(path), strict=False)
-
-        shot = self.dataset.shot
-        way = self.dataset.way
-        if shot == 1:
-            class_fracs = [(k+1)/way for k in range(0,way)]
-            shot_fracs = [1.0]
-        elif shot == 5:
-            class_fracs = [k/way for k in [1, 3, 5]]
-            shot_fracs = [k/shot for k in [1, 3, 5]]
-        elif shot == 10:
-            class_fracs = [k/way for k in [1, 3, 5]]
-            shot_fracs = [k/shot for k in [1, 3, 5, 10]]
-        else:
-            print("ERROR - Unsupported shot/way for vary swap attack")
-            return
-
-        gen_clean_accuracies = []
-        clean_accuracies = []
-        clean_target_as_context_accuracies = []
-        # num_tasks = min(2, self.dataset.get_num_tasks())
-        num_tasks = min(self.dataset.get_num_tasks(), self.args.attack_tasks)
-        for task in tqdm(range(num_tasks), dynamic_ncols=True):
-            with torch.no_grad():
-                context_images, context_labels, target_images, target_labels = self.dataset.get_clean_task(task, self.device)
-                gen_clean_accuracies.append(self.calc_accuracy(context_images, context_labels, target_images, target_labels))
-
-
-                eval_images, eval_labels = self.dataset.get_eval_task(task, self.device)
-                # Evaluate on independent target sets
-                for k in range(len(eval_images)):
-                    eval_imgs_k = eval_images[k].to(self.device)
-                    eval_labels_k = eval_labels[k].to(self.device)
-                    clean_accuracies.append(self.calc_accuracy(context_images, context_labels, eval_imgs_k, eval_labels_k))
-                    clean_target_as_context_accuracies.append(self.calc_accuracy(target_images, target_labels, eval_imgs_k, eval_labels_k))
-
-        self.print_average_accuracy(gen_clean_accuracies, "Gen setting: Clean accuracy", "")
-        self.print_average_accuracy(clean_accuracies, "Clean accuracy", "")
-        self.print_average_accuracy(clean_target_as_context_accuracies, "Clean Target as Context accuracy", "")
-
-        for class_frac in class_fracs:
-            for shot_frac in shot_fracs:
-                #Do the swap attack
-                frac_descrip = "{}_ac_{}_ash".format(class_frac, shot_frac)
-
-                # Accuracies for evaluation setting
-                adv_context_accuracies = []
-                adv_target_accuracies = []
-                adv_target_as_context_accuracies = []
-                adv_context_as_target_accuracies = []
-
-                gen_adv_context_accuracies = []
-                gen_adv_target_accuracies = []
-
-                for task in tqdm(range(num_tasks), dynamic_ncols=True):
-                    with torch.no_grad():
-                        context_images, context_labels, target_images, target_labels = self.dataset.get_clean_task(task, self.device)
-                        adv_target_images, target_labels = self.dataset.get_frac_adversarial_set(task, self.device, class_frac, shot_frac, set_type="target")
-                        adv_context_images, context_labels = self.dataset.get_frac_adversarial_set(task, self.device, class_frac, shot_frac, set_type="context")
-
-                        # Evaluate in normal/generation setting
-                        # Doesn't account for collusion
-                        gen_adv_context_accuracies.append(self.calc_accuracy(adv_context_images, context_labels, target_images, target_labels))
-                        gen_adv_target_accuracies.append(self.calc_accuracy(context_images, context_labels, adv_target_images, target_labels))
-
-                        eval_images, eval_labels = self.dataset.get_eval_task(task, self.device)
-
-                        # Evaluate on independent target sets
-                        for k in range(len(eval_images)):
-                            eval_imgs_k = eval_images[k].to(self.device)
-                            eval_labels_k = eval_labels[k].to(self.device)
-
-                            adv_context_accuracies.append(self.calc_accuracy(adv_context_images, context_labels, eval_imgs_k, eval_labels_k))
-                            adv_target_accuracies.append(self.calc_accuracy(eval_imgs_k, eval_labels_k, adv_target_images, target_labels))
-
-                            adv_target_as_context_accuracies.append(self.calc_accuracy(adv_target_images, target_labels, eval_imgs_k, eval_labels_k))
-                            adv_context_as_target_accuracies.append(self.calc_accuracy(eval_imgs_k, eval_labels_k, adv_context_images, context_labels))
-
-                self.print_average_accuracy(gen_adv_context_accuracies, "Gen setting: Context attack accuracy", frac_descrip)
-                self.print_average_accuracy(gen_adv_target_accuracies, "Gen setting: Target attack accuracy", frac_descrip)
-
-                self.print_average_accuracy(adv_context_accuracies, "Context attack accuracy", frac_descrip)
-                self.print_average_accuracy(adv_target_as_context_accuracies, "Adv Target as Context accuracy", frac_descrip)
-                self.print_average_accuracy(adv_target_accuracies, "Target attack accuracy", frac_descrip)
-                self.print_average_accuracy(adv_context_as_target_accuracies, "Adv Context as Target", frac_descrip)
-
-
-    def meta_dataset_attack_swap(self, path, session):
-        return
-
-
-    def attack_swap(self, path, session):
-        return
-
-
-    def attack_homebrew(self, path, session):
-        return
-
     def prepare_task(self, task_dict, shuffle=True):
         context_images_np, context_labels_np = task_dict['context_images'], task_dict['context_labels']
-        all_target_images_np, all_target_labels_np = task_dict['target_images'], task_dict['target_labels']
+        target_images_np, target_labels_np = task_dict['target_images'], task_dict['target_labels']
 
         context_images_np = context_images_np.transpose([0, 3, 1, 2])
 
@@ -532,23 +400,17 @@ class Learner:
         context_images = torch.from_numpy(context_images_np)
         context_labels = torch.from_numpy(context_labels_np)
 
-        all_target_images_np = all_target_images_np.transpose([0, 3, 1, 2])
+        target_images_np = target_images_np.transpose([0, 3, 1, 2])
         if shuffle:
-            all_target_images_np, all_target_labels_np = self.shuffle(all_target_images_np, all_target_labels_np)
-        all_target_images = torch.from_numpy(all_target_images_np)
-        all_target_labels = torch.from_numpy(all_target_labels_np)
-        all_target_labels = all_target_labels.type(torch.LongTensor)
-
-	
-        target_images, target_labels = all_target_images, all_target_labels
+            target_images_np, target_labels_np = self.shuffle(target_images_np, target_labels_np)
+        target_images = torch.from_numpy(target_images_np)
+        target_labels = torch.from_numpy(target_labels_np)
+        target_labels = target_labels.type(torch.LongTensor)
 
         context_images = context_images.to(self.device)
         target_images = target_images.to(self.device)
         target_labels = target_labels.to(self.device)
         context_labels = context_labels.to(self.device)
-
-        #target_images_np = all_target_images_np
-        #extra_datasets = (context_images_np, target_images_np, None, None)
 
         return context_images, target_images, context_labels, target_labels
 
