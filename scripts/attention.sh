c638dd3351c591e604a0d7169758b98ba656b81b
cravingoxygen <vze.ezv@gmail.com>
2021-04-08 13:00:20 +0100

    Fixed up the script to test, and made scripts for training dot product attention protonets

--- scripts/attention.sh
+++ scripts/attention.sh
@@ -1,4 +1,18 @@
 ulimit -n 50000
 export PYTHONPATH=.
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=1
+
+
+python3 ./learners/protonets_attention/src/main.py \
+    --classifier protonets_attention \
+    --mode test \
+    --test_datasets ilsvrc_2012 \
+    --data_path /scratch/jfb54/tf-meta-dataset/records \
+    --test_model_path /scratch/etv21/protonets_attention/checkpoints_1/fully_trained.pt \
+    --pretrained_resnet_path learners/protonets_attention/models/pretrained_resnet.pt.tar \
+    --batch_normalization basic \
+    --feature_adaptation film \
+    --max_support_test 50 \
+    --max_way_test 5 \
+    --dataset meta-dataset_ilsvrc_only 
 
---
+++ scripts/quick_train_attention.sh
@@ -0,0 +1,19 @@
+ulimit -n 50000
+export PYTHONPATH=.
+export CUDA_VISIBLE_DEVICES=1
+
+
+python3 ./learners/protonets_attention/src/main.py \
+    --classifier protonets_attention \
+    --data_path /scratch/jfb54/tf-meta-dataset/records \
+    --tasks_per_batch 16 \
+    -c /scratch/etv21/protonets_attention/checkpoints_1 \
+    --batch_normalization basic \
+    --feature_adaptation film \
+    -i 300 \
+    --max_support_train 300 \
+    --dataset meta-dataset_ilsvrc_only \
+    --attention_temperature 2.0 \
+    -lr 0.0005 \
+    --pretrained_resnet_path learners/protonets_attention/models/pretrained_resnet.pt.tar 
+
