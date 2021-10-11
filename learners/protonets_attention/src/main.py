Fixed error with inconsistent query/context labelling when using the Value-Tracking dataset wrapper. Also fixed bug with entropy tracking and off-by-one error in the make_noisy labels function

learners/protonets_attention/src/main.py
@@ -46,6 +46,7 @@ def np_set_to_torch(images_np, labels_np, shuffle=False):
         images_np, labels_np = shuffle_set(images_np, labels_np)
     images = torch.from_numpy(images_np)
     labels = torch.from_numpy(labels_np)
+    return images, labels
     
     
 def shuffle_set(images, labels):
@@ -620,10 +621,13 @@ class Learner:
         ranking_mode = self.args.importance_mode  
         save_out_interval = 500
         if self.args.tasks <= 1000:
-            #if self.args.top_k == 1:
-            #    save_out_interval = 1
-            #else:
             save_out_interval = 100
+            '''
+            if self.args.top_k == 1:
+                save_out_interval = 1
+            else:
+                save_out_interval = 100
+            '''
 
         for item in self.test_set:
             accuracies = []
@@ -644,9 +648,10 @@ class Learner:
                     task_accuracy = self.accuracy_fn(target_logits, target_labels).item()
                     accuracies.append(task_accuracy)
                     del target_logits
-                import pdb; pdb.set_trace()
-                probs = context_labels.sum(dim=0)/float(len(context_labels.unique()))
-                entropies.append(Categorical(probs=probs).entropy)
+
+                one_hot = torch.nn.functional.one_hot(context_labels).to("cpu")
+                probs = one_hot.sum(dim=0)/float(len(context_labels))
+                entropies.append(Categorical(probs=probs).entropy())
                 
                 # Save the target/context features
                 # We could optimize by only doing this if we're doing attention or divine selection, but for now whatever, it's a single forward pass
@@ -684,6 +689,7 @@ class Learner:
                 if ti % save_out_interval == 0:
                     self.save_image_set(ti, context_images[candidate_indices], "keep_{}".format(ti), labels=context_labels[candidate_indices])
                     self.save_image_set(ti, context_images[dropped_indices], "discard_{}".format(ti), labels=context_labels[dropped_indices])
+                    self.save_image_set(ti, target_images, "target_{}".format(ti), labels=target_labels)
                     
                 if ti < self.args.tasks -1:
                     # Request new points to replace those
@@ -698,8 +704,8 @@ class Learner:
                     # Remove the old points, replace with new points
 
                     # Get new query set as well
-                    target_images, target_labels, _ = self.dataset.get_query_set()
-                    target_images, target_labels = move_set_to_cuda(target_images ,target_labels, self.device)
+                    #target_images, target_labels, _ = self.dataset.get_query_set()
+                    #target_images, target_labels = move_set_to_cuda(target_images ,target_labels, self.device)
 
             self.print_and_log_metric(accuracies, item, 'Accuracy')
             self.save_image_set(ti, context_images, "context_final".format(ti), labels=context_labels)
@@ -716,7 +722,7 @@ class Learner:
         
     def bar_plot_and_log(self, keys, vals, descrip, filename, bar=False):
         self.logger.log(descrip)
-        for key, val in zip(keys, values):
+        for key, val in zip(keys, vals):
             self.logger.log("{} : {}".format(key, val))
         plt.bar(keys, vals)
         plt.savefig(os.path.join(self.args.checkpoint_dir, filename))
@@ -1005,7 +1011,7 @@ class Learner:
             # Select images from the context set randomly to be mislabeled 
             mislabeled_indices = rng.choice(num_context_images, num_noisy, replace=False)
             # Mislabels selected images randomly
-            new_context_labels[mislabeled_indices] = (new_context_labels[mislabeled_indices] + rng.integers(0, num_classes, num_noisy) + 1) % num_classes
+            new_context_labels[mislabeled_indices] = (new_context_labels[mislabeled_indices] + rng.integers(1, num_classes, num_noisy)) % num_classes
         elif self.args.noise_type == "ood":
             # We want num_noisy/total patterns = error rate, where total patterns = num_clean + num_noisy
             # And num_clean = num_at_start/2

learners/protonets_attention/src/split_wrapper.py
@@ -81,7 +81,7 @@ class IdentifiableDatasetWrapper:
             self.query_data = tv_datasets.MNIST(dataset_path, transform=transforms, train=False, download=True)
         else:
             print("Unsupported dataset specified: {}".format(dataset_name))
-
+        '''
         nc = 2
         ns = 15
         self.context_data.classes = self.context_data.classes[0:nc]
@@ -94,6 +94,7 @@ class IdentifiableDatasetWrapper:
         self.query_data.targets = self.query_data.targets[0:ns*5]
         for i in range(ns*5):
             self.query_data.targets[i] = i % nc
+        '''
 
         self.query_mapping = map_to_classes(self.query_data)
         # Now we want splits for this per class so we can construct tasks
@@ -144,9 +145,14 @@ class IdentifiableDatasetWrapper:
         # Use our custom, order-preserving unique function
         chosen_classes = unique(unnormalized_context_labels)
         task_dict["target_images"], unnormalized_target_labels, task_dict["target_ids"] = self._construct_query_set(chosen_classes)
-        # Here we make the asssumption that all classes are represented equally in the context and target sets (but the whole setup assumes that)
-        norm_labels = torch.arange(len(chosen_classes))
-        task_dict["context_labels"], task_dict["target_labels"] = norm_labels.repeat_interleave(self.shot), norm_labels.repeat_interleave(self.query_shot)
+        
+        # If all classes are represented, don't renormalize
+        if len(chosen_classes) == len(self.context_data.classes):
+            task_dict["context_labels"], task_dict["target_labels"] = unnormalized_context_labels, unnormalized_target_labels
+        else:
+            # Here we make the asssumption that all classes are represented equally in the context and target sets (but the whole setup assumes that)
+            norm_labels = torch.arange(len(chosen_classes))
+            task_dict["context_labels"], task_dict["target_labels"] = norm_labels.repeat_interleave(self.shot), norm_labels.repeat_interleave(self.query_shot)
 
         return task_dict
         
@@ -231,10 +237,10 @@ class ValueTrackingDatasetWrapper(IdentifiableDatasetWrapper):
         # Mark the selected context images so that we don't swap them in multiple times
         for i, context_id in enumerate(self.current_context_ids):
             self.drawable_context_ids.remove(context_id)
-            if task_dict["context_labels"][i] in self.returned_label_counts:
-                self.returned_label_counts[task_dict["context_labels"][i]] += 1
+            if task_dict["context_labels"][i].item() in self.returned_label_counts:
+                self.returned_label_counts[task_dict["context_labels"][i].item()] += 1
             else:
-                self.returned_label_counts[task_dict["context_labels"][i]] = 1
+                self.returned_label_counts[task_dict["context_labels"][i].item()] = 1
         return task_dict
         
     def mark_discarded(self, image_ids):
@@ -268,10 +274,10 @@ class ValueTrackingDatasetWrapper(IdentifiableDatasetWrapper):
         context_labels = torch.zeros(num_points_requested, dtype=torch.long)
         for i, id in enumerate(indices):
             context_images[i], context_labels[i] = self.context_data[id]
-            if context_labels[i] in self.returned_label_counts:
-                self.returned_label_counts[context_labels[i]] += 1
+            if context_labels[i].item() in self.returned_label_counts:
+                self.returned_label_counts[context_labels[i].item()] += 1
             else:
-                self.returned_label_counts[context_labels[i]] = 1
+                self.returned_label_counts[context_labels[i].item()] = 1
         return context_images, context_labels, indices
         
     def get_query_set(self):