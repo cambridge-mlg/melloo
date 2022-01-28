cb8fcea539795d15b22f8075e58e43d2e8daa476
Elre <vze.ezv@gmail.com>
2022-01-28 10:38:22 +0000

    Added proper support for tracking standard deviations for scaling logits in protonets. This is now an initialization parameter and the class responds correctly depending on its value. Also added some helper functions in cases where the prototypes should be saved to the model (for efficient loo) or not. Also added an inefficient version of loo where stds are recalculated with each loo.

---
+++ toy_cifar10/mahalanobis.py
@@ -0,0 +1,126 @@
+import torch
+from helper_classes import euclidean_metric
+"""
+The code in this file is substantially based on the code for "Improved Few-Shot Visual Classification"
+by Peyman Bateni, Raghav Goyal, Vaden Masrani1, Frank Wood, and Leonid Sigal
+that can be found here: https://github.com/peymanbateni/simple-cnaps
+
+In turn, this file has been borrowed from: https://raw.githubusercontent.com/cambridge-mlg/LITE/main/src/mahalanonbis.py
+"""
+
+class MahalanobisPredictor(nn.Module):
+    def __init__(self):
+        super(MahalanobisPredictor, self).__init__()
+        return
+
+    def forward(self, context_features, context_labels, target_features):
+        """
+        Forward pass through the model for one episode.
+        :param context_images: (torch.tensor) Images in the context set (batch x C x H x W).
+        :param context_labels: (torch.tensor) Labels for the context set (batch x 1 -- integer representation).
+        :param target_images: (torch.tensor) Images in the target set (batch x C x H x W).
+        :return: (torch.tensor) Categorical distribution on label set for each image in target set (batch x num_labels).
+        """
+        means, precisions = self._compute_class_means_and_precisions(context_features, context_labels)
+
+        print("Prototype distances: {}".format(euclidean_metric(means, means)[0][1].item()))
+        return self.predict(target_features)
+
+    def predict(self, target_features):
+        return self._predict(target_features, self.prototypes, self.precisions)
+
+    def _predict(self, target_features, class_means, class_precision_matrices):
+        # grabbing the number of classes and query examples for easier use later in the function
+        number_of_classes = class_means.size(0)
+        number_of_targets = target_features.size(0)
+
+        """
+        Calculating the Mahalanobis distance between query examples and the class means
+        including the class precision estimates in the calculations, reshaping the distances
+        and multiplying by -1 to produce the sample logits
+        """
+        repeated_target = target_features.repeat(1, number_of_classes).view(-1, class_means.size(1))
+        repeated_class_means = class_means.repeat(number_of_targets, 1)
+        repeated_difference = (repeated_class_means - repeated_target)
+        repeated_difference = repeated_difference.view(number_of_targets, number_of_classes,
+                                                       repeated_difference.size(1)).permute(1, 0, 2)
+        first_half = torch.matmul(repeated_difference, class_precision_matrices)
+        logits = torch.mul(first_half, repeated_difference).sum(dim=2).transpose(1, 0) * -1
+
+        return logits
+
+    def loo(self, loo_features, loo_labels, target_features, way):
+        means, precisions = self._compute_class_means_and_precisions(context_features, context_labels, save_prototypes=False)
+        return self._predict(target_features, means, target_features, precisions)
+
+    def _compute_class_means_and_precisions(self, features, labels, save_prototypes=True):
+        means = []
+        precisions = []
+        task_covariance_estimate = self._estimate_cov(features)
+        for c in torch.unique(labels):
+            # filter out feature vectors which have class c
+            class_features = torch.index_select(features, 0, self._extract_class_indices(labels, c))
+            # mean pooling examples to form class means
+            means.append(self._mean_pooling(class_features).squeeze())
+            lambda_k_tau = (class_features.size(0) / (class_features.size(0) + 1))
+            precisions.append(torch.inverse(
+                (lambda_k_tau * self._estimate_cov(class_features)) + ((1 - lambda_k_tau) * task_covariance_estimate)\
+                + torch.eye(class_features.size(1), class_features.size(1)).cuda(0)))
+
+        means = (torch.stack(means))
+        precisions = (torch.stack(precisions))
+
+        if save_prototypes:
+            self.prototypes = means
+            self.precisions = precisions
+
+        return means, precisions
+
+    @staticmethod
+    def _estimate_cov(examples, rowvar=False, inplace=False):
+        """
+        SCM: function based on the suggested implementation of Modar Tensai
+        and his answer as noted in: https://discuss.pytorch.org/t/covariance-and-gradient-support/16217/5
+
+        Estimate a covariance matrix given data.
+
+        Covariance indicates the level to which two variables vary together.
+        If we examine N-dimensional samples, `X = [x_1, x_2, ... x_N]^T`,
+        then the covariance matrix element `C_{ij}` is the covariance of
+        `x_i` and `x_j`. The element `C_{ii}` is the variance of `x_i`.
+
+        Args:
+            examples: A 1-D or 2-D array containing multiple variables and observations.
+                Each row of `m` represents a variable, and each column a single
+                observation of all those variables.
+            rowvar: If `rowvar` is True, then each row represents a
+                variable, with observations in the columns. Otherwise, the
+                relationship is transposed: each column represents a variable,
+                while the rows contain observations.
+
+        Returns:
+            The covariance matrix of the variables.
+        """
+        if examples.dim() > 2:
+            raise ValueError('m has more than 2 dimensions')
+        if examples.dim() < 2:
+            examples = examples.view(1, -1)
+        if not rowvar and examples.size(0) != 1:
+            examples = examples.t()
+        factor = 1.0 / (examples.size(1) - 1)
+        if inplace:
+            examples -= torch.mean(examples, dim=1, keepdim=True)
+        else:
+            examples = examples - torch.mean(examples, dim=1, keepdim=True)
+        examples_t = examples.t()
+        return factor * examples.matmul(examples_t).squeeze()
+
+    @staticmethod
+    def _extract_class_indices(labels, which_class):
+        class_mask = torch.eq(labels, which_class)  # binary mask of labels equal to which_class
+        class_mask_indices = torch.nonzero(class_mask)  # indices of labels equal to which class
+        return torch.reshape(class_mask_indices, (-1,))  # reshape to be a 1D vector
+
+    @staticmethod
+    def _mean_pooling(x):
+        return torch.mean(x, dim=0, keepdim=True)
--- toy_cifar10/protonets.py
+++ toy_cifar10/protonets.py
@@ -1,25 +1,19 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from helper_classes import euclidean_metric
 
 cluster_distance = 800
-
-def euclidean_metric(target_features, class_prototypes):
-    num_target_features = target_features.shape[0]
-    num_prototypes = class_prototypes.shape[0]
-
-    distances = (target_features.unsqueeze(1).expand(num_target_features, num_prototypes, -1) -
-                 class_prototypes.unsqueeze(0).expand(num_target_features, num_prototypes, -1)).pow(2).sum(dim=2)
-    return -distances
     
 class ProtoNets(nn.Module):
     """
     Main model class.
     :param args: (Argparser) Arparse object containing model hyper-parameters.
     """
-    def __init__(self, way):
+    def __init__(self, way, scale_by_std=False):
         super(ProtoNets, self).__init__()
         self.way = way
+        self.scale_by_std = scale_by_std
 
     def forward(self, context_features, context_labels, target_features):
         """
@@ -34,20 +28,25 @@ class ProtoNets(nn.Module):
         prototypes = self._compute_prototypes(context_features, context_labels, way)
 
         print("Prototype distances: {}".format(euclidean_metric(prototypes, prototypes)[0][1].item()))
-        logits = euclidean_metric(target_features, self.prototypes)
-        return logits
+        return self.predict(target_features)
         
-    def classify(self, target_features):
-        logits = euclidean_metric(target_features, self.prototypes)
+    # Will always used saved protoypes and stds
+    def predict(self, target_features):
+        return self._predict(target_features, self.prototypes, self.stds)
+        
+    # Will use specified prototypes and stds
+    def _predict(self, target_features, class_prototypes, class_stds):
+        logits = euclidean_metric(target_features, class_prototypes)
         # Scale by squared standard deviation
-        #logits = logits/(self.std_devs**2).to(logits.device)
+        if self.scale_by_std:
+            logits = logits/(class_stds**2).to(logits.device)
         return logits
 
-    def _compute_prototypes(self, context_features, context_labels, way):
+    def _compute_prototypes(self, context_features, context_labels, way, save_prototypes=True):
         prototypes = []
         prototype_labels = []
         prototype_counts = []
-        cluster_std_devs = []
+        cluster_stds = []
         #unpertured_prototype_distance = np.sqrt(cluster_distance)/2.0
         #unperturbed_symmetric_coords = unpertured_prototype_distance/np.sqrt(2)
         #prototype_0 = np.array([unperturbed_symmetric_coords, unperturbed_symmetric_coords])
@@ -62,21 +61,31 @@ class ProtoNets(nn.Module):
                 class_mean = torch.Tensor(prototype_1).unsqueeze(0).to(context_features.device)
             '''
 
-            class_stddev = torch.std(class_features)
-            #class_stddev = torch.Tensor([1]).to(context_features.device) #torch.std(class_features)
+            class_std = torch.std(class_features)
+            #class_std = torch.Tensor([1]).to(context_features.device) #torch.std(class_features)
             prototypes.append(class_mean)
-            cluster_std_devs.append(class_stddev)
+            cluster_stds.append(class_std)
             prototype_labels.append(c)
             prototype_counts.append(len(class_features))
 
-        self.prototypes = torch.squeeze(torch.stack(prototypes))
-        self.prototype_labels = prototype_labels
-        self.prototype_counts = prototype_counts
-        self.std_devs = torch.Tensor(cluster_std_devs)
+        if save_prototypes:
+            self.prototypes = torch.squeeze(torch.stack(prototypes))
+            self.prototype_labels = prototype_labels
+            self.prototype_counts = prototype_counts
+            self.stds = torch.Tensor(cluster_stds)
         
-        return self.prototypes
+        return prototypes, stds
+        
         
     def loo(self, loo_features, loo_labels, target_features, way):
+        if self.scale_by_std:
+            return self._loo_std_scale(loo_features, loo_labels, target_features, way)
+        else:
+            return self._loo_efficient(loo_features, loo_labels, target_features, way)
+        
+    # Subtract the given points from the means. i.e. given points should be left out
+    # Doesn't account for potential change in stds
+    def _loo_efficient(self, loo_features, loo_labels, target_features, way):
         prototypes = self.prototypes.clone()
         # Remove the given features from the computed prototypes
         for c in torch.unique(loo_labels):
@@ -109,7 +118,14 @@ class ProtoNets(nn.Module):
             other_prototype = ((other_prototype * other_prototype_count) + torch.sum(class_features, dim=0)) / (other_prototype_count + len(loo_features))
             prototypes[other_prototype_index] = other_prototype
 
-        logits = euclidean_metric(target_features, prototypes)
+        logits = self._predict(target_features, prototypes, self.stds)
+        return logits
+        
+        
+    # Recalculate all values with the given points. i.e. points are ones remaining after the ones to be left out have been removed
+    def _loo_std_scale(self, loo_features, loo_labels, target_features, way):
+        new_prototypes, new_stds = self._compute_prototypes(loo_features, loo_labels, target_features, way, save_prototypes=False)
+        logits = self._predict(target_features, new_prototypes, new_stds)
         return logits
 
     @staticmethod
