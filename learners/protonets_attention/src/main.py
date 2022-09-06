Added KL divergence-based LOO (including relevant helper funcion in utils). Minor fixes that arose from the refactor. Made it possible to give spread constraints to selection_by_dropping. Made the value calculation functions optionally do aggregation on the results already (since that's how we use it anyway). No longer save state on the mahalanobis-based model.

learners/protonets_attention/src/main.py
@@ -11,21 +11,13 @@ import utils
 from model import FewShotClassifier
 from normalization_layers import TaskNormI
 from dataset import get_dataset_reader
-import matplotlib.pyplot as plt
-from PIL import Image
 import pickle
 import math
-import matplotlib
-import matplotlib.pyplot as plt
 import gc
-import matplotlib.patches as mpatches
-from sklearn.manifold import TSNE
-import sklearn.metrics
+
 import metaloo
 import metrics
 
-from scipy.stats import kendalltau
-from sklearn.metrics.pairwise import rbf_kernel
 
 NUM_VALIDATION_TASKS = 200
 PRINT_FREQUENCY = 1000
@@ -46,7 +38,7 @@ class Learner:
         self.logger.print_and_log("Options: %s\n" % self.args)
         self.logger.print_and_log("Checkpoint Directory: %s\n" % self.log_files.checkpoint_dir)
         
-        self.metrics = new metrics.Metrics(self.args.checkpoint_dir, self.args.way, self.args.top_k, self.logger)
+        self.metrics = metrics.Metrics(self.args.checkpoint_dir, self.args.way, self.args.top_k, self.logger)
         self.representer_args = {
                 "l2_regularize_classifier": self.args.l2_regularize_classifier,
                 "l2_lambda": self.args.l2_lambda,
@@ -84,7 +76,7 @@ class Learner:
         elif self.args.task_type == "generate_coreset":
             assert self.args.selection_mode != "drop"
         else:
-            assert self.args.spread_constraint == "none"
+            assert self.args.spread_constraint == "none" or self.args.spread_constraint == "nonempty"
             if self.args.task_type == "noisy_shots" or self.args.task_type == "generate_coreset_discard":
                 assert self.args.selection_mode == "drop"
             else:
@@ -189,13 +181,13 @@ class Learner:
         parser.add_argument("--top_k", type=int, default=2, help="How many points to retain from each class")
         parser.add_argument("--selection_mode", choices=["top_k", "multinomial", "divine", "drop"], default="top_k",
                             help="How to select the candidates from the importance weights. Drop option only works for noisy shot selection at present.")
-        parser.add_argument("--importance_mode", choices=["attention", "loo", "representer", "random", "all"], default="all",
+        parser.add_argument("--importance_mode", choices=["attention", "loo", "representer", "random", "kl_loo", "all"], default="all",
                             help="How to calculate candidate importance")
         parser.add_argument("--kernel_agg", choices=["sum", "sum_abs", "class"], default="class",
                             help="When using representer importance, how to aggregate kernel information")
         # Shot selection settings
-        parser.add_argument("--spread_constraint", choices=["by_class", "none"], default="by_class",
-                            help="Spread coresets over classes (by_class) or no constraint (none)")
+        parser.add_argument("--spread_constraint", choices=["by_class", "nonempty", "none"], default="none",
+                            help="Spread coresets over classes (by_class) or by not allowing it to drop the last instance of a class (nonempty) or no constraint (none)")
         # Noisy task settings
         parser.add_argument("--drop_rate", type=float, default=0.1, help="Percentage of points to drop (as float in [0,1])")
         parser.add_argument("--noise_type", choices=["mislabel", "ood"], default="mislabel",
@@ -258,7 +250,7 @@ class Learner:
             if self.args.task_type == "generate_coreset":
                 self.generate_coreset(self.args.test_model_path)
             elif self.args.task_type == "generate_coreset_discard":
-                self.generate_coreset_discard(self.args.test_model_path)
+                self.discard_to_generate_coreset(self.args.test_model_path)
                 #self.save_coreset_from_ranking(self.args.test_model_path)
             elif self.args.task_type == "noisy_shots":
                 self.detect_noisy_shots(self.args.test_model_path)
@@ -417,8 +409,12 @@ class Learner:
             self.logger.print_and_log('{0:}: {1:3.1f}+/-{2:2.1f}'.format(item, accuracy, accuracy_confidence))
             
 
-    def random_weights(self, context_images, context_labels, target_images, target_labels):
-        return torch.tensor(np.random.rand(len(target_labels), len(context_labels)))
+    def random_weights(self, context_images, context_labels, target_images, target_labels, by_query_point=True):
+        weights_per_qp = torch.tensor(np.random.rand(len(target_labels), len(context_labels)))
+        if (by_query_point):
+            return weights_per_qp
+        weights = weights_per_qp.sum(dim=0)
+        return weights
 
     
 
@@ -439,8 +435,7 @@ class Learner:
 
             self.metrics.save_image_set(0, context_images, "selected_{}".format(key))
 
-    def generate_coreset_discard(self, path):
-        #import pdb; pdb.set_trace()
+    def discard_to_generate_coreset(self, path):
         self.logger.print_and_log("")  # add a blank line
         self.logger.print_and_log('Generating coreset (by discard) using model {0:}: '.format(path))
         self.model = self.init_model()
@@ -463,7 +458,7 @@ class Learner:
             entropies = []
             restart_checkpoints = []
             if self.args.importance_mode == 'all':
-                self.metrics.plot_scatter("Error - coreset construction by discard does not support doing all importance modes simultaneously")
+                self.metrics.print_and_log_metric("Error - coreset construction by discard does not support doing all importance modes simultaneously")
                 return  
             # Start with the original, random context set
             task_dict = self.dataset.get_test_task(item)
@@ -496,24 +491,26 @@ class Learner:
                         context_features, target_features = self.model.context_features.clone(), self.model.target_features.clone()
 
                 if ranking_mode == 'loo':
-                    weight_per_qp = metaloo.calculate_loo(self.model, self.loss, context_images, context_labels, target_images, target_labels)
+                    weights = metaloo.calculate_loo(self.model, self.loss, context_images, context_labels, target_images, target_labels, by_query_point=False)
                 elif ranking_mode == 'attention':
                     # Make a copy of the attention weights otherwise we get a reference to what the model is storing
                     with torch.no_grad():
                         self.model(context_images, context_labels, target_images, target_labels, MetaLearningState.META_TEST)
                     attention_weights = self.model.attention_weights.copy()
-                    weight_per_qp = self.reshape_attention_weights(attention_weights, context_labels, len(target_labels)).cpu()
+                    weights = self.reshape_attention_weights(attention_weights, context_labels, len(target_labels), by_query_point=False).cpu()
                 elif ranking_mode == 'representer':
-                    weight_per_qp = metaloo.calculate_representer_values(self.model, self.loss, context_images, context_labels, context_features, target_labels, target_features, self.representer_args, self.representer_args)
+                    weights = metaloo.calculate_representer_values(self.model, self.loss, context_images, context_labels, context_features, target_labels, target_features, self.representer_args, self.representer_args, by_query_point=False)
                 elif ranking_mode == 'random':
-                    weight_per_qp = self.random_weights(context_images, context_labels, target_images, target_labels)
+                    weights = self.random_weights(context_images, context_labels, target_images, target_labels, by_query_point=False)
+                elif ranking_mode == 'kl_loo':
+                    weights = metaloo.calculate_kl_loo(self.model, self.model.get_classifier_params(), context_images, context_labels, target_images, target_labels)
                     
                 # May want to normalize here:
-                weights = weight_per_qp.sum(dim=0)
+                #weights = weight_per_qp.sum(dim=0)
                 rankings = torch.argsort(weights, descending=True)
                 
                 # Discard least valuable point/s
-                candidate_indices, dropped_indices = metaloo.select_by_dropping(weights, number=self.args.top_k)
+                candidate_indices, dropped_indices = metaloo.select_by_dropping(weights, number=self.args.top_k, spread_constraint=self.args.spread_constraint, class_labels=context_labels)
                 candidate_ids = image_ids[candidate_indices]
                 dropped_ids = image_ids[dropped_indices]
                 if len(dropped_indices) == 1:
@@ -618,9 +615,9 @@ class Learner:
             all_eval_predictions = torch.cat(eval_predictions)
             self.metrics.plot_confusion_matrix(all_eval_labels, pred_labels=all_eval_predictions, filename="confusion_eval.png")
             
-            self.metrics.plot_scatter(accuracies, item, 'Accuracy')
-            self.metrics.plot_scatter(eval_accuracies, item, 'Eval Accuracy')
-            self.metrics.plot_scatter(eval_losses, item, 'Eval Loss')
+            self.metrics.print_and_log_metric(accuracies, item, 'Accuracy')
+            self.metrics.print_and_log_metric(eval_accuracies, item, 'Eval Accuracy')
+            self.metrics.print_and_log_metric(eval_losses, item, 'Eval Loss')
             self.logger.print_and_log("Number of no_drop iterations: {}".format(no_drop_count))
             self.metrics.save_image_set(ti, context_images, "context_final".format(ti), labels=context_labels)
             self.metrics.plot_and_log(accuracies, "Accuracies over tasks", "accuracies.png")
@@ -704,7 +701,7 @@ class Learner:
                         image_rankings = metaloo.add_image_rankings(image_rankings, task_dict["context_ids"][i], context_images[i], mode, normalized_weights[i])
 
             #import pdb; pdb.set_trace()
-            self.metrics.plot_scatter(accuracies_full, item, 'Full Accuracy')
+            self.metrics.print_and_log_metric(accuracies_full, item, 'Full Accuracy')
 
             # Righty-oh, now that we have our image_rankings, we need to do something with them.
             # Let's get some basic stats about them;
@@ -735,7 +732,7 @@ class Learner:
 
                         del target_logits
                     
-                self.metrics.plot_scatter(eval_accuracies, item, 'Eval Accuracy ({})'.format(mode))
+                self.metrics.print_and_log_metric(eval_accuracies, item, 'Eval Accuracy ({})'.format(mode))
 
     def _funky_bimodal_inner_loop(self, context_images, context_labels, context_labels_orig, target_images, target_labels, ranking_modes, accuracies, num_unique_labels, ti, descrip):
         full_accuracy = -1
@@ -869,16 +866,16 @@ class Learner:
             for key in num_unique_labels_bimodal.keys():
                 unique_np = np.array(num_unique_labels_bimodal[key])
                 self.logger.print_and_log("Average number of unique labels (bimodal) ({}): {}+/-{}".format(key, unique_np.mean(), unique_np.std()))
-            self.metrics.plot_scatter(accuracies_full_bimodal, item, 'Accuracy (bimodal)')
+            self.metrics.print_and_log_metric(accuracies_full_bimodal, item, 'Accuracy (bimodal)')
             for key in accuracies_bimodal.keys():
-                self.metrics.plot_scatter(accuracies_bimodal[key], item, 'Accuracy (bimodal) {}'.format(key))
+                self.metrics.print_and_log_metric(accuracies_bimodal[key], item, 'Accuracy (bimodal) {}'.format(key))
                 
             for key in num_unique_labels_unimodal.keys():
                 unique_np = np.array(num_unique_labels_unimodal[key])
                 self.logger.print_and_log("Average number of unique labels (unimodal) ({}): {}+/-{}".format(key, unique_np.mean(), unique_np.std()))
-            self.metrics.plot_scatter(accuracies_full_unimodal, item, 'Accuracy (unimodal)')
+            self.metrics.print_and_log_metric(accuracies_full_unimodal, item, 'Accuracy (unimodal)')
             for key in accuracies_unimodal.keys():
-                self.metrics.plot_scatter(accuracies_unimodal[key], item, 'Accuracy (unimodal) {}'.format(key))
+                self.metrics.print_and_log_metric(accuracies_unimodal[key], item, 'Accuracy (unimodal) {}'.format(key))
            
     def select_shots(self, path):
         self.logger.print_and_log("")  # add a blank line
@@ -1029,19 +1026,19 @@ class Learner:
                     unique_np = np.array(num_unique_labels[key])
                     self.logger.print_and_log("Average number of unique labels ({}): {}+/-{}".format(key, unique_np.mean(), unique_np.std()))
 
-                self.metrics.plot_scatter(accuracies_orig_full, item, '*Accuracy (Not bimodal)')
+                self.metrics.print_and_log_metric(accuracies_orig_full, item, '*Accuracy (Not bimodal)')
                 for key in accuracies_orig_by_mode.keys():
-                    self.metrics.plot_scatter(accuracies_orig_by_mode[key], item, '*Accuracy (Not bimodal) {}'.format(key))
+                    self.metrics.print_and_log_metric(accuracies_orig_by_mode[key], item, '*Accuracy (Not bimodal) {}'.format(key))
                 
-            self.metrics.plot_scatter(accuracies_full, item, 'Accuracy')
+            self.metrics.print_and_log_metric(accuracies_full, item, 'Accuracy')
 
             for key in weights.keys():
-                self.metrics.plot_scatter(accuracies[key], item, 'Accuracy {}'.format(key))
+                self.metrics.print_and_log_metric(accuracies[key], item, 'Accuracy {}'.format(key))
             if self.args.importance_mode == 'all':
                 for key in correlations:
-                    self.metrics.plot_scatter(correlations[key], item, 'Correlation {}'.format(key))
+                    self.metrics.print_and_log_metric(correlations[key], item, 'Correlation {}'.format(key))
                 for key in intersections:
-                    self.metrics.plot_scatter(intersections[key], item, 'Intersections {}'.format(key))
+                    self.metrics.print_and_log_metric(intersections[key], item, 'Intersections {}'.format(key))
         
     def make_noisy(self, task_dict):
         num_classes = len(np.unique(task_dict["target_labels"]))
@@ -1276,12 +1273,12 @@ class Learner:
                         self.metrics.plot_hist(context_labels[removed_indices], class_bins, "selected_label_hist_flipped", ti, 'Classes selected for dropping', 'Presented class label', 'Num points selected for drop')
 
             if len(accuracies_clean) > 0:
-                self.metrics.plot_scatter(accuracies_clean, item, 'Clean Accuracy')
-            self.metrics.plot_scatter(accuracies_noisy, item, 'Noisy Accuracy')
+                self.metrics.print_and_log_metric(accuracies_clean, item, 'Clean Accuracy')
+            self.metrics.print_and_log_metric(accuracies_noisy, item, 'Noisy Accuracy')
 
             for key in ranking_modes:
-                self.metrics.plot_scatter(accuracies[key], item, 'Accuracy {}'.format(key))
-                self.metrics.plot_scatter(overlaps[key], item, 'Shot Overlap {}'.format(key))
+                self.metrics.print_and_log_metric(accuracies[key], item, 'Accuracy {}'.format(key))
+                self.metrics.print_and_log_metric(overlaps[key], item, 'Shot Overlap {}'.format(key))
 
 
     def reshape_attention_weights(self, attention_weights, context_labels, num_target_points):
@@ -1294,7 +1291,10 @@ class Learner:
             for q in range(c_weights.shape[0]):
                 weights_per_query_point[q][c_indices] = c_weights[q]
                 
-        return weights_per_query_point
+        if by_query_point:
+            return weights_per_query_point
+        weights = weights_per_query_point.sum(dim=0)
+        return weights
 
 
     def _augment(self, num_augment_per_class, images, labels):

learners/protonets_attention/src/metaloo.py
@@ -1,6 +1,8 @@
 import utils
 import numpy as np
 import torch
+from utils import MetaLearningState
+
             
 def select_multinomial(number, weights, class_labels, spread_constraint):
     if spread_constraint == "by_class":
@@ -66,7 +68,7 @@ def select_top_sum_redun_k(k, weights, embeddings, gamma=25, plus_div=False):
         selected_indices += [cand]
     return selected_indices, weight_terms, diversity_terms
 
-def select_by_dropping(weights, drop_rate=None, number=None):
+def select_by_dropping(weights, drop_rate=None, number=None, spread_constraint=None, class_labels=None):
     if (drop_rate != None and number != None):
         print("When selecting by dropping, only number to drop or drop rate can be specified, not both")
         return None
@@ -78,8 +80,37 @@ def select_by_dropping(weights, drop_rate=None, number=None):
     else:
         num_to_keep = int(len(weights) * (1 - drop_rate))
     ranking = torch.argsort(weights, descending=True)
-    return ranking[0:num_to_keep], ranking[num_to_keep:]
+    if spread_constraint == None or spread_constraint == "none":
+        return ranking[0:num_to_keep], ranking[num_to_keep:]
 
+    if spread_constraint == "by_class":
+        print("By class spread constraint not yet supported when selecting by dropping x")
+        return
+
+    if spread_constraint == "nonempty":
+        # Only dropping 1 point
+        if num_to_keep != len(ranking)-1:
+            print("nonempty spread constraint currenlty only support dropping 1 point at a time")
+            return None
+        drop_index = num_to_keep
+        keep_mask = torch.ones(len(ranking), dtype=bool)
+        keep_mask[drop_index] = False
+
+        total_num_classes = len(class_labels.unique())
+        num_represented_classes = len(class_labels[ranking[keep_mask]].unique())
+        while num_represented_classes != total_num_classes:
+            import pdb; pdb.set_trace()
+
+            # We shouldn't reach the end of the loop as long as the way/shot setup makes sense
+            keep_mask[drop_index] = True
+            drop_index -= 1
+            keep_mask[drop_index] = False
+
+            num_represented_classes = len(class_labels[ranking[keep_mask]].unique())
+        drop_mask = torch.logical_not(keep_mask)
+        return ranking[keep_mask], ranking[drop_mask]
+
+# class_labels must be specified if spread_constraint isn't "none"
 def select_top_k(number, weights, spread_constraint, class_labels=None):
     if spread_constraint == "by_class":
         classes = torch.unique(class_labels)
@@ -94,10 +125,25 @@ def select_top_k(number, weights, spread_constraint, class_labels=None):
     elif spread_constraint == "none":
         ranking = torch.argsort(weights, descending=True)
         return ranking[0:number]
-        
-        
+    elif spread_constraint == "nonempty":
+        print("By class spread constraint not yet supported when selecting by dropping x")
+        return None
+        """
+        end_index = number
+        indices = ranking[0:end_index]
+        total_num_classes = len(class_labels.unique())
+        num_represented_classes = len(class_labels[indices].unique())
+        while num_represented_classes != total_num_classes:
+            end_index -= 1
+            # We shouldn't reach the end of the loop as long as the way/shot setup makes sense
+            indices = torch.cat(ranking[0:end_index], ranking[end_index+1:])
+            num_represented_classes = len(class_labels[indices].unique())
+        return ranking[indices]
+        """
+
+
 def calculate_representer_values(model, loss, context_images, context_labels, context_features, target_labels, target_features,     
-        params):
+        params, by_query_point=True):
     l2_regularize_classifier = params["l2_regularize_classifier"]
     l2_lambda = params["l2_lambda"]
     kernel_agg = params["kernel_agg"]
@@ -147,7 +193,11 @@ def calculate_representer_values(model, loss, context_images, context_labels, co
     else:
         print("Unsupported kernel aggregation method specified")
         return None
-    return representers_agg
+    if by_query_point:
+        return representers_agg
+
+    weights = representers_agg.sum(dim=0)
+    return weights
 
 def alternative_loo(model, loss, context_images, context_labels, target_images, target_labels, way):
 
@@ -173,7 +223,7 @@ def alternative_loo(model, loss, context_images, context_labels, target_images, 
             logits = model(context_images_loo, context_labels_loo, target_images, target_labels, MetaLearningState.META_TEST)
             loss = loss(logits, target_labels)
             weights[i] = loss
-            
+
     return weights
     
 def remove_unrepresented_points(candidate_labels, target_images, target_labels):
@@ -197,7 +247,38 @@ def remove_unrepresented_points(candidate_labels, target_images, target_labels):
         reduced_candidate_labels[reduced_candidate_labels > unc] = reduced_candidate_labels[reduced_candidate_labels > unc] - 1
     return reduced_candidate_labels, reduced_target_images, reduced_labels  
 
-def calculate_loo(model, loss,  context_images, context_labels, target_images, target_labels):
+def calculate_kl_loo(model, initial_parameters,  context_images, context_labels, target_images, target_labels):
+    num_classes = len(context_labels.unique())
+    kl_change = torch.zeros(len(context_labels))
+    mu1, precision_matrix1 = initial_parameters["class_means"], initial_parameters["class_precision_matrices"]
+    with torch.no_grad():
+         for i in range(context_images.shape[0]):
+            if i == 0:
+                context_images_loo = context_images[i + 1:]
+                context_labels_loo = context_labels[i + 1:]
+            else:
+                context_images_loo = torch.cat((context_images[0:i], context_images[i + 1:]), 0)
+                context_labels_loo = torch.cat((context_labels[0:i], context_labels[i + 1:]), 0)
+            # Don't technically need to do a full forward pass, since we don't care about the loss. 
+            if len(context_labels_loo.unique()) != num_classes:
+                # Don't try dropping this point; it is the last one of the class
+                kl_change[i] = 1000 * num_classes
+                continue
+
+            model(context_images_loo, context_labels_loo, target_images, target_labels, MetaLearningState.META_TEST)
+            loo_params = model.get_classifier_params()
+            mu2, precision_matrix2 = loo_params["class_means"], loo_params["class_precision_matrices"]
+            for c in range(num_classes):
+                
+                class_distance = utils.kl_divergence(mu1[c], precision_matrix1[c], mu2[c], precision_matrix2[c])
+                kl_change[i] += class_distance.cpu()
+            # Maybe we should only add distance for class it belongs to?
+            #kl_change[i] = distance
+
+    return kl_change
+
+
+def calculate_loo(model, loss,  context_images, context_labels, target_images, target_labels, by_query_point=True):
     target_classes = target_labels.unique()
     loss_per_qp = torch.zeros((len(target_labels), len(context_labels)))
     with torch.no_grad():
@@ -237,10 +318,13 @@ def calculate_loo(model, loss,  context_images, context_labels, target_images, t
             
     #Somehow turn the loss into a weight:
     #weight_per_qp = loss_per_qp/loss_per_qp.max() - 1
-    return loss_per_qp 
-    
-    
-    
+    if by_query_point:
+        return loss_per_qp
+    weights = loss_per_qp.sum(dim=0)
+    return weights
+
+
+
 def add_image_rankings(image_ranking_dict, image_key, image, ranking_key, ranking):
     if image_key not in image_ranking_dict.keys():
        image_ranking_dict[image_key] = {}

learners/protonets_attention/src/metrics.py
@@ -1,5 +1,14 @@
 import utils
 
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import sklearn.metrics
+from sklearn.manifold import TSNE
+from scipy.stats import kendalltau
+import numpy as np
+import torch
+import os
 
 
 class Metrics:
@@ -11,7 +20,7 @@ class Metrics:
         self.logger = logger
         
         
-    def save_image_set(task_num, images, descrip, labels=None):
+    def save_image_set(self, task_num, images, descrip, labels=None):
         if task_num != -1:
             path = os.path.join(self.checkpoint_dir, str(task_num))
         else:
@@ -29,7 +38,7 @@ class Metrics:
                 image_path = os.path.join(path, '{}_index_{}_label_{}.png'.format(descrip, i, labels[i]))
             utils.save_image(tmp_images[i], image_path)
 
-    def plot_scatter(x, y, x_label, y_label, plot_title, output_name, 
+    def plot_scatter(self, x, y, x_label, y_label, plot_title, output_name, 
         class_labels=None, color=None, split_by_class_label=False, x_min=None, x_max=None, y_min=None, y_max=None):
         class_colors = ["#dc0f87", "#e8b90e", "#29e414", "#f76b1f", "#585d9c"]
         x = utils.convert_to_numpy(x)
@@ -95,7 +104,7 @@ class Metrics:
         
         return ave_corr, ave_intersected
         
-    def plot_confusion_matrix(true_labels, filename, logits=None, pred_labels=None):
+    def plot_confusion_matrix(self, true_labels, filename, logits=None, pred_labels=None):
         labels = true_labels.cpu()
         if logits is not None:
             preds = logits.argmax(axis=1).cpu()
@@ -109,14 +118,14 @@ class Metrics:
         plt.savefig(os.path.join(self.checkpoint_dir, filename))
         plt.close()
             
-    def plot_and_log(vals, descrip, filename):
+    def plot_and_log(self, vals, descrip, filename):
         self.logger.log(descrip)
         self.logger.log("{}".format(vals))
         plt.plot(vals)
         plt.savefig(os.path.join(self.checkpoint_dir, filename))
         plt.close()
         
-    def bar_plot_and_log(keys, vals, descrip, filename):
+    def bar_plot_and_log(self, keys, vals, descrip, filename):
         self.logger.log(descrip)
         for key, val in zip(keys, vals):
             self.logger.log("{} : {}".format(key, val))
@@ -125,7 +134,7 @@ class Metrics:
         plt.close()
     
 
-    def plot_hist(x, bins, filename, task_num=None, title='', x_label='', y_label='', density=False):
+    def plot_hist(self, x, bins, filename, task_num=None, title='', x_label='', y_label='', density=False):
         x = utils.convert_to_numpy(x)
         plt.hist(x, bins=bins, density=density)
         plt.xlabel(x_label)
@@ -139,7 +148,7 @@ class Metrics:
         plt.close()
 
 
-    def plot_tsne(points, labels, prototypes, descrip):
+    def plot_tsne(self, points, labels, prototypes, descrip):
         class_colors = ["#dc0f87", "#e8b90e", "#29e414", "#f76b1f", "#585d9c", "#323ca8", "#ff0303", "#03ffc4", "#afff03", "#a566e3"]
         prototypes = utils.convert_to_numpy(prototypes)
         prototype_labels = utils.convert_to_numpy(torch.unique(labels)) # Need the ordering of torch.unique

learners/protonets_attention/src/model.py
@@ -35,7 +35,7 @@ class FewShotClassifier(nn.Module):
         self.feature_adaptation_network = networks.get_feature_adaptation()
         self.task_representation = None
         self.feature_extractor_params = {}
-        self.class_representations = OrderedDict()  # Dictionary mapping class label (integer) to encoded representation
+        #self.class_representations = OrderedDict()  # Dictionary mapping class label (integer) to encoded representation
         self.context_features = None
         self.target_features = None
         self.classifier_params = None
@@ -46,8 +46,8 @@ class FewShotClassifier(nn.Module):
         if self.args.classifier == "protonets_cross_transformer":
             self.cross_attention = CrossTransformer(temperature=self.args.attention_temperature)
             
-        if self.args.classifier == "protonets_mahalanobis":
-            self.class_precision_matrices = OrderedDict() # Dictionary mapping class label (integer) to regularized precision matrices estimated
+        #if self.args.classifier == "protonets_mahalanobis":
+        #    self.class_precision_matrices = OrderedDict() # Dictionary mapping class label (integer) to regularized precision matrices estimated
 
 
 
@@ -79,25 +79,19 @@ class FewShotClassifier(nn.Module):
             logits = self._cross_transformer_euclidiean_distances(prototypes, target_embeddings, h, w)
 
         elif self.args.classifier == "protonets_mahalanobis":
-            logits = _protonets_mahalanobis_classifier(self.context_features, self.target_features, context_labels)
+            logits = self._protonets_mahalanobis_classifier(self.context_features, self.target_features, context_labels)
         else:
             print("Unsupported model type requested")
             return
 
         return logits
-        
+
     def _protonets_mahalanobis_classifier(self, context_features, target_features, context_labels):
         """
         SCM: we build both class representations and the regularized covariance estimates.
         """
-        # clear all dictionaries from previous forward pass
-        self.class_representations.clear()
-        self.class_precision_matrices.clear()
-        
         # get the class means and covariance estimates in tensor form
-        self._build_class_reps_and_covariance_estimates(context_features, context_labels)
-        class_means = torch.stack(list(self.class_representations.values())).squeeze(1)
-        class_precision_matrices = torch.stack(list(self.class_precision_matrices.values())) 
+        class_means, class_precision_matrices = self._build_class_reps_and_covariance_estimates(context_features, context_labels)
         self.classifier_params = {
             'class_means': class_means,
             'class_precision_matrices': class_precision_matrices
@@ -121,7 +115,7 @@ class FewShotClassifier(nn.Module):
         logits = torch.mul(first_half, repeated_difference).sum(dim=2).transpose(1, 0) * -1
         return logits
     
-    def set_class_prototypes(prototypes):
+    def set_class_prototypes(self, prototypes):
         self.classifier_params = {"class_prototypes": prototypes}
     
     def _build_class_reps_and_covariance_estimates(self, context_features, context_labels):
@@ -136,13 +130,15 @@ class FewShotClassifier(nn.Module):
         SCM: calculating a task level covariance estimate using the provided function.
         """
         task_covariance_estimate = self.estimate_cov(context_features)
+        class_representations = OrderedDict()
+        class_precision_matrices = OrderedDict()
         for c in torch.unique(context_labels):
             # filter out feature vectors which have class c
             class_features = torch.index_select(context_features, 0, extract_class_indices(context_labels, c))
             # mean pooling examples to form class means
             class_rep = mean_pooling(class_features)
             # updating the class representations dictionary with the mean pooled representation
-            self.class_representations[c.item()] = class_rep
+            class_representations[c.item()] = class_rep
             """
             Calculating the mixing ratio lambda_k_tau for regularizing the class level estimate with the task level estimate."
             Then using this ratio, to mix the two estimate; further regularizing with the identity matrix to assure invertability, and then
@@ -150,9 +146,14 @@ class FewShotClassifier(nn.Module):
             dictionary for use later in infering of the query data points.
             """
             lambda_k_tau = (class_features.size(0) / (class_features.size(0) + 1))
-            self.class_precision_matrices[c.item()] = torch.inverse(
+            class_precision_matrices[c.item()] = torch.inverse(
                 (lambda_k_tau * self.estimate_cov(class_features)) + ((1 - lambda_k_tau) * task_covariance_estimate) \
                 + torch.eye(class_features.size(1), class_features.size(1)).cuda(0))
+
+        class_means = torch.stack(list(class_representations.values())).squeeze(1)
+        class_precs = torch.stack(list(class_precision_matrices.values())) 
+
+        return class_means, class_precs
         
     def estimate_cov(self, examples, rowvar=False, inplace=False):
         """